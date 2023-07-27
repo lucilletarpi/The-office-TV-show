29011b115039ffd16de1e0f3906a6918f3a30942
lucilletarpi <98407181+lucilletarpi@users.noreply.github.com>
2023-07-27 18:28:13 +0200

    api found first data set up reflection on how to organize better

--- data.js
+++ data.js
@@ -1,16 +1,40 @@
 function listOffice() {
-	// Efface l'ancienne recherche
-	// selectResult.innerHTML = "";
+	const divEpisodeSeason1 = document.querySelector(".listEpisodes"); // Utilisez l'id correct ici
 
-	const apiUrl = "http://localhost:3000/api/office";
+	console.log(divEpisodeSeason1);
+	const options = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization:
+				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjExZGYwYTIxMGIyYWI4MjU2YjU1YmZjNzkwNTA5OCIsInN1YiI6IjY0YzBmMzBjMmYxYmUwMDE0ZWY1YzA2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PLURScAQ52WfKCV6D51NYU8LHKMxcICd1jyWJHPova0",
+		},
+	};
 
-	fetch(apiUrl)
+	fetch("https://api.themoviedb.org/3/tv/2316/season/1", options)
 		.then((response) => response.json())
-		.then((data) => {
-			console.log(data); // Les informations de la série seront affichées ici
+		.then((response) => {
+			console.log(response.episodes);
+			for (let i = 0; i < response.episodes.length; i++) {
+				// Assurez-vous de commencer par l'index 0
+				console.log(response.episodes[i]);
+				const numListEpisode = document.createElement("li");
+				numListEpisode.innerHTML =
+					"Episode" + " " + response.episodes[i].episode_number;
+				numListEpisode.className = "numbEpisode";
+				const titleEpisode = document.createElement("h2");
+				titleEpisode.innerHTML =
+					"Titre de l'épisode" + ": " + response.episodes[i].name;
+				titleEpisode.className = "titleEpisode";
+				const summaryEpisode = document.createElement("p");
+				summaryEpisode.innerHTML = "Résumé";
+				numListEpisode.appendChild(titleEpisode);
+
+				divEpisodeSeason1.appendChild(numListEpisode);
+			}
 		})
-		.catch((error) => {
-			console.error("Erreur lors de la requête vers le serveur:", error);
-		});
+		.catch((err) => console.error(err));
 }
-listOffice();
+document.addEventListener("DOMContentLoaded", function () {
+	listOffice();
+});
