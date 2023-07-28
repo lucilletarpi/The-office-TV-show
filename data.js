e6b688ad71321e3bad9d5469ce99ed975d05dbc4
lucilletarpi <98407181+lucilletarpi@users.noreply.github.com>
2023-07-28 09:47:49 +0200

    mise en place du résumé et dynamisation des données de l'api voir pour les pages

--- data.js
+++ data.js
@@ -1,6 +1,8 @@
 function listOffice() {
-	const divEpisodeSeason1 = document.querySelector(".listEpisodes"); // Utilisez l'id correct ici
-
+	const divEpisodeSeason1 = document.querySelector(".listEpisodes");
+	const saison = document.getElementById("saison");
+	const valeur = saison.textContent;
+	console.log(valeur);
 	console.log(divEpisodeSeason1);
 	const options = {
 		method: "GET",
@@ -10,27 +12,34 @@ function listOffice() {
 				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjExZGYwYTIxMGIyYWI4MjU2YjU1YmZjNzkwNTA5OCIsInN1YiI6IjY0YzBmMzBjMmYxYmUwMDE0ZWY1YzA2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PLURScAQ52WfKCV6D51NYU8LHKMxcICd1jyWJHPova0",
 		},
 	};
-
-	fetch("https://api.themoviedb.org/3/tv/2316/season/1", options)
+	// season info
+	fetch("https://api.themoviedb.org/3/tv/2316/season/${valeur}", options)
 		.then((response) => response.json())
 		.then((response) => {
 			console.log(response.episodes);
 			for (let i = 0; i < response.episodes.length; i++) {
-				// Assurez-vous de commencer par l'index 0
 				console.log(response.episodes[i]);
+				//LI creation for episode number
 				const numListEpisode = document.createElement("li");
 				numListEpisode.innerHTML =
 					"Episode" + " " + response.episodes[i].episode_number;
 				numListEpisode.className = "numbEpisode";
+				//
+				// h2 creation for episode title
 				const titleEpisode = document.createElement("h2");
 				titleEpisode.innerHTML =
 					"Titre de l'épisode" + ": " + response.episodes[i].name;
 				titleEpisode.className = "titleEpisode";
+				//
+				// p creation for episode summary
 				const summaryEpisode = document.createElement("p");
-				summaryEpisode.innerHTML = "Résumé";
+				summaryEpisode.className = "summaryEpisode";
+				summaryEpisode.innerHTML =
+					"Résumé" + " : " + response.episodes[i].overview;
+				//
 				numListEpisode.appendChild(titleEpisode);
-
 				divEpisodeSeason1.appendChild(numListEpisode);
+				titleEpisode.appendChild(summaryEpisode);
 			}
 		})
 		.catch((err) => console.error(err));
